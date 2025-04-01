071e88dee0738da131e36b4430fd7dd145b21f5b
sajeebro <sajithmadusanka720@gmail.com>
2025-04-01 21:55:49 +0530

    added payment-service

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Transport } from '@nestjs/microservices';
+import { join } from 'path';
+
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.GRPC,
+    options: {
+      package: 'payment',
+      protoPath: join(__dirname, '../payment.proto'),
+      url: 'localhost:50056',
+    },
+  });
+  app.enableShutdownHooks();
+  await app.listen();
+  console.log('Payment service is running on: http://localhost:50056');
+}
+void bootstrap();
